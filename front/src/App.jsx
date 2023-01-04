db0c33311d01ec27d30ee9d627855de3577c5fd0
fvilmot <fvilmot@gmail.com>
2023-01-04 18:14:06 +0100

    ajout de la navbar

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Toys from "./routes/Toys";
 import Toy from "./routes/Toy";
+import Navbar from "./routes/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Toys />} />
         <Route path="/toy/:id" element={<Toy />} />
---
+++ front/src/routes/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+const Navbar = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to="/">Home</Link>
+          </Typography>
+          <Button color="inherit">Add toy</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
