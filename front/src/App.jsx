5be236cd243871b9c4ad9cdc8457a2d48f1592cd
fvilmot <fvilmot@gmail.com>
2023-01-04 15:11:20 +0100

    Installation de MUI et mise en page des toys

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,15 @@
 import { useState, useEffect } from "react";
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material/";
+
 function App() {
   const [toys, setToys] = useState([]);
   const [categories, setCategories] = useState([]);
@@ -14,10 +24,36 @@ function App() {
 
   return (
     <div>
-      <h1>Toys</h1>
-      {toys.map((toy) => {
-        return <p>{toy.name}</p>;
-      })}
+      <TableContainer
+        component={Paper}
+        sx={{ minWidth: 650, width: "90%", margin: "50px auto" }}
+      >
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Toys</TableCell>
+              <TableCell align="left">description</TableCell>
+              <TableCell align="right">price</TableCell>
+              <TableCell align="right">category</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {toys.map((toy) => (
+              <TableRow
+                key={toy.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {toy.name}
+                </TableCell>
+                <TableCell align="left">{toy.description}</TableCell>
+                <TableCell align="right">{toy.price}</TableCell>
+                <TableCell align="right">{toy.category_id}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
